67696561eabfeb837b922b34d349519fe73cf4fd
Jory <email": null>
2015-09-29 16:57:06 +1300

    Add facebook login

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  def self.find_or_create_from_omniauth(auth)
+    user = User.where({:uid => auth[:uid], :provider => auth[:provider]}).first_or_create
+
+    info = auth['extra']['raw_info']
+
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.name = info.name
+    user.save if user.changed?
+    user
+  end
+end
