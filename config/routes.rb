8973da84fa0e659cad03fc60662f837a2f6263e0
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-09-30 10:59:23 +1300

    midway through integration need to commit to merge features from master

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+
+	root 'site#index'
+
 	namespace :api do
 		namespace :v1 do
   			resources :reports, except: [:edit, :new]
 		end
 	end
 end
+
--- public/bundle.js
+++ public/bundle.js
@@ -1,3 +1,39 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+L.mapbox.accessToken = 'pk.eyJ1IjoicGV0dHljcmltZSIsImEiOiJjaWY0cTBoZDgwbXl0c2RtN2ZjYzhicjZoIn0.FDjxXktw-rA-U-qobjyNxQ';
+var map = L.mapbox.map(document.getElementById('map'), 'mapbox.streets')
+    .setView([-41.29, 174.78], 13);
+
+
+
+var click = document.getElementById('click')
+map.on('click', function(e) {
+	var latlng = [e.latlng.lng, e.latlng.lat]
+	$.featherlight($('#example'));
+	});
+
+
+function dropPin(coord){
+	var y = L.mapbox.featureLayer({
+	    // this feature is in the GeoJSON format: see geojson.org
+	    // for the full specification
+	    // need to make this object relate to the object in the db somehow???
+	    type: 'Feature',
+	    geometry: {
+	        type: 'Point',
+	        // coordinates here are in longitude, latitude order because
+	        // x, y is the standard for GeoJSON and many formats
+	        coordinates: coord
+	    },
+	    properties: {
+	        title: '',
+	        description: '',
+	        'marker-size': 'large',
+	        'marker-color': '#BE9A6B',
+	        'marker-symbol': 'cafe'
+	    }
+	}).addTo(map);
+	console.log(y)
+
+}
 
 },{}]},{},[1]);
