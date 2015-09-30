e2210cf47ebcf3a7a9c8ba696cbb283f391ecb00
Jory <joryakuhata@gmail.com>
2015-09-30 15:18:52 +1300

    Batmap comic styling

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoicGV0dHljcmltZSIsImEiOiJjaWY0cTBoZDgwbXl0c2RtN2ZjYzhicjZoIn0.FDjxXktw-rA-U-qobjyNxQ';
-var map = L.mapbox.map(document.getElementById('map'), 'mapbox.streets')
+var map = L.mapbox.map(document.getElementById('map'), 'pettycrime.nj17g72j')
     .setView([-41.29, 174.78], 13);
 
 var latlng = []
+
 
 
 
