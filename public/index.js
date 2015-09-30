f84bee875c6423a2f2667e71bdc69e1ae650b1f5
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2015-10-01 11:50:43 +1300

    Add some React stuff

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var Modal = require('react-modal');
+var ModalForm = require('./source/form')
+var PoliceNumber = require('./source/police')
+
 
 var appElement = document.getElementById('example');
 
@@ -8,8 +11,12 @@ Modal.setAppElement('#example');
 var App = React.createClass({
 
   getInitialState: function() {
-    return { modalIsOpen: false };
+    return {
+      modalIsOpen: false,
+      lifeThreatening: false
+    };
   },
+
 
   openModal: function() {
     this.setState({modalIsOpen: true});
@@ -20,33 +27,30 @@ var App = React.createClass({
   },
 
   handleModalCloseRequest: function() {
-    // opportunity to validate something and keep the modal open even if it
-    // requested to be closed
     this.setState({modalIsOpen: false});
   },
 
   handleInputChange: function() {
-    this.setState({foo: 'bar'});
+    this.setState({lifeThreatening: !this.state.lifeThreatening})
   },
 
   render: function() {
+    console.log('Modal: props, state', this.props, this.state)
     return (
-      <div>
-        <button onClick={this.openModal}>Report a crime</button>
+      <div className="buttonform">
+        <img src={'/assets/batman-button.png'} alt="batman" className="img-responsive" onClick={this.openModal} />
         <Modal
-          closeTimeoutMS={150}
+          closeTimeoutMS={100}
           isOpen={this.state.modalIsOpen}
           onRequestClose={this.handleModalCloseRequest}>
-          <h1>Hello</h1>
-          <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
+          <h1>Is your crime life threatening?</h1>
           <form>
-            <input onChange={this.handleInputChange} />
-            <input />
-            <input />
-            <input />
-            <input />
+            <input type="radio" name="crime" value="yes" onChange={this.handleInputChange} checked={this.state.lifeThreatening}>Yes</input>
+            <input type="radio" name="crime" value="no" onChange={this.handleInputChange} checked={!this.state.lifeThreatening}>No</input>
+            <br/>
           </form>
+          <button className='button-close' onClick={this.closeModal}>close</button>
+          {this.state.lifeThreatening ? <PoliceNumber /> : <ModalForm closeModal={this.closeModal} />}
         </Modal>
       </div>
     );
@@ -54,3 +58,4 @@ var App = React.createClass({
 });
 
 React.render(<App/>, appElement);
+
---
+++ public/source/police.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function() {
+		return(
+			<div className="police">
+				<h2>Call the police</h2>
+        <p>Phone: 04-381 2000</p>					
+			</div>
+		)
+	}
+})
