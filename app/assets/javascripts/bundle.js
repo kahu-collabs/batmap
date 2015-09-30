042f3bcf4491cea9d48761d9d38f8d729f14fcd0
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-09-30 16:56:21 +1300

    eggyfarts ARE GO

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -3,6 +3,7 @@ L.mapbox.accessToken = 'pk.eyJ1IjoicGV0dHljcmltZSIsImEiOiJjaWY0cTBoZDgwbXl0c2RtN
 var map = L.mapbox.map(document.getElementById('map'), 'mapbox.streets')
     .setView([-41.29, 174.78], 13);
 
+console.log("eggyfarts");
 var latlng = []
 
 
