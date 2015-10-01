636f92a26c5f3a2794a2c132a612f52e2260e747
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2015-10-02 11:46:27 +1300

    Add facebook login to model

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, '965680136882580', "cae70daf53bee0d44881822f9d0bb6d7"
+  provider :facebook, "965680136882580", "cae70daf53bee0d44881822f9d0bb6d7"
 end
