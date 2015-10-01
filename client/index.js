c5901d12e368a91765e9cd7a5c1d1165771c7959
Jory <email": null>
2015-10-01 20:57:25 +1300

    omg its persisting and the pin drops are working javascript is my bitch

--- client/index.js
+++ client/index.js
@@ -1,15 +1,24 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoicGV0dHljcmltZSIsImEiOiJjaWY0cTBoZDgwbXl0c2RtN2ZjYzhicjZoIn0.FDjxXktw-rA-U-qobjyNxQ';
+
+var geoJson = []
 var map = L.mapbox.map(document.getElementById('map'), 'pettycrime.nj17g72j')
     .setView([-41.29, 174.78], 13);
 
+var myLayer = L.mapbox.featureLayer().addTo(map);
 var latlng = []
 
+
+
+render();
 
 
 
 
 var click = document.getElementById('click')
+
 map.on('click', function(e) {
+	console.log(e.latlng.lng)
+	console.log(e.latlng.lat)
 	latlng = [e.latlng.lng, e.latlng.lat]
 	console.log(latlng)
 	$.featherlight($('#example'));
@@ -23,7 +32,7 @@ $('#example').submit(function(event){
 
 
 	var to_db = {category_type: type, description: event.target[1].value, happened_before: event.target[2].checked, location: latlng.join() };
-
+	console.log("onclick " + to_db["category_type"])
 	submitCrime(to_db);
 
 
@@ -44,13 +53,12 @@ function testType(type){
 }
 
 function submitCrime(input){
-	var data = input.location
 
 	$.ajax({
 	  type: "POST",
 	  url: "api/v1/reports",
 	  data: input,
-	  success: dropPin(data),
+	  success: dropPin(input),
 	  dataType: "json"
 	});
 
@@ -63,43 +71,98 @@ function submitCrime(input){
 
 
 
-"properties": {
-           "title": "mugging",
-           "icon": {
-               "iconUrl": "batpin.png",
-               "iconSize": [50, 50], // size of the icon
-               "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
-               "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
-               "className": "dot"
-           }
 
-function dropPin(coord){
-	var y = L.mapbox.featureLayer({
-	    // this feature is in the GeoJSON format: see geojson.org
-	    // for the full specification
-	    // need to make this object relate to the object in the db somehow???
-	    type: 'Feature',
-	    geometry: {
-	        type: 'Point',
-	        // coordinates here are in longitude, latitude order because
-	        // x, y is the standard for GeoJSON and many formats
-	        coordinates: latlng
-	    },
-	    properties: {
-	        title: 'Mugging',
-	        description: 'asdf',
-	        "icon": {
-               "iconUrl": "batpin.png",
-               "iconSize": [50, 50], // size of the icon
-               "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
-               "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
-               "className": "dot"
-           }
-	        'marker-size': 'large',
-	        'marker-color': '#BE9A6B',
-	        'marker-symbol': 'cafe'
-	    }
-	}).addTo(map);
-	console.log(y)
+function dropPin(object_with_data){
 
+		var crimeObjects = [{
+		       "type": "Feature",
+		       "geometry": {
+		           "type": "Point",
+		           "coordinates": latlng
+		       },
+		       "properties": {
+		           "title": "Joker Gassing",
+		           "icon": {
+		               "iconUrl": "assets/joker_pin.png",
+		               "iconSize": [50, 50], // size of the icon
+		               "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
+		               "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
+		               "className": "dot"
+		           }
+		       }
+		   }, {
+		       "type": "Feature",
+		       "geometry": {
+		           "type": "Point",
+		           "coordinates": latlng
+		       },
+		       "properties": {
+		           "title": "Mugging",
+		           "icon": {
+		               "iconUrl": "assets/batpin.png",
+		               "iconSize": [50, 50],
+		               "iconAnchor": [25, 25],
+		               "popupAnchor": [0, -25],
+		               "className": "dot"
+		           }
+		       }
+		   }, {
+		       "type": "Feature",
+		       "geometry": {
+		           "type": "Point",
+		           "coordinates": latlng
+		       },
+		       "properties": {
+		           "title": "Home Invasion",
+		           "icon": {
+		               "iconUrl": "assets/home_invasion.png",
+		               "iconSize": [50, 50],
+		               "iconAnchor": [25, 25],
+		               "popupAnchor": [0, -25],
+		               "className": "dot"
+		           }
+		       }
+		   }, {
+		       "type": "Feature",
+		       "geometry": {
+		           "type": "Point",
+		           "coordinates": latlng
+		       },
+		       "properties": {
+		           "title": "Car Theft",
+		           "icon": {
+		               "iconUrl": "assets/car_thieft.png",
+		               "iconSize": [50, 50],
+		               "iconAnchor": [25, 25],
+		               "popupAnchor": [0, -25],
+		               "className": "dot"
+		           }
+		       }
+		   }];
+
+
+
+
+	x = (object_with_data["category_type"]) - 1;
+
+	geoJson.push(crimeObjects[x])
+	console.log(geoJson);
+	render()
+
+
+}
+
+
+function render(){
+	myLayer.on('layeradd', function(e) {
+    var marker = e.layer,
+        feature = marker.feature;
+
+   marker.setIcon(L.icon(feature.properties.icon));
+
+	});
+
+
+
+	myLayer.setGeoJSON(geoJson);
 }
